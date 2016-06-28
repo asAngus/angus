259a65046bc444512782fdcf8fa0da1788821eac
wei_peng <wei_peng@cloudfin.com>
2016-06-28 11:28:49 +0800

    增加数据库导入

---
+++ examples/java-examples/src/main/java/com/cmm/tools/dbimport/DBImport.java
@@ -0,0 +1,135 @@
+/**
+ * Title: DBImport.java<br/>
+ * Description: <br/>
+ * Copyright: Copyright (c) 2016<br/>
+ * Company: cloudfn<br/>
+ *
+ */
+package com.cmm.tools.dbimport;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+/**
+ * Title: DBImport<br/>
+ * Description: <br/>
+ * Company: cloudfn<br/>
+ * 
+ * @author weipeng
+ * @date 2016年6月28日上午10:29:35
+ *
+ */
+public class DBImport {
+
+    /**
+     * Title: main<br/>
+     * Description: <br/>
+     * 
+     * @author weipeng
+     * @date 2016年6月28日上午10:29:35
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        // 设定数据库驱动，数据库连接地址、端口、名称，用户名，密码
+        String driverName = "oracle.jdbc.driver.OracleDriver";
+        String url = "jdbc:oracle:thin:@112.96.28.45:8006/wowealth"; // test为数据库名称，1521为连接数据库的默认端口
+        String user = "payadm"; // aa为用户名
+        String password = "Z9!&81f^o"; // 123为密码
+
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        // 数据库连接对象
+        Connection conn = null;
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader("./test.cvs"));
+            String readLine = reader.readLine();
+            String[] split = readLine.split(",");
+            Map<String, List<String>> tmp = new HashMap<>();
+            for (String string : split) {
+                List<String> list = tmp.get(split[0]);
+                if (list == null) {
+                    list = new ArrayList<>();
+                    tmp.put(split[0], list);
+                }
+                list.add(string);
+            }
+            // 反射Oracle数据库驱动程序类
+            Class.forName(driverName);
+
+            // 获取数据库连接
+            conn = DriverManager.getConnection(url, user, password);
+
+            // 输出数据库连接
+            System.out.println(conn);
+
+            // 定制sql命令
+            String sql = "Insert into MKMOUTSCOREPRDDETAIL (PRD_ID,CODE_STS,FAV_CODE,TM_SMP) values (?,?,?,to_char(sysdate,'yyyymmddhhmiss'))";
+            String del = "delete MKMOUTSCOREPRDDETAIL where prd_id=?";
+            // 创建该连接下的PreparedStatement对象
+            pstmt = conn.prepareStatement(sql);
+            PreparedStatement delPstmt = conn.prepareStatement(del);
+            Set<Entry<String, List<String>>> entrySet = tmp.entrySet();
+            for (Entry<String, List<String>> entry : entrySet) {
+                String key = entry.getKey();
+                delPstmt.setString(1, key);
+                delPstmt.executeUpdate();
+                List<String> value = entry.getValue();
+                int i = 1;
+                for (String string : value) {
+                    pstmt.setString(i++, string);
+                }
+                pstmt.addBatch();
+            }
+
+            // 执行查询语句，将数据保存到ResultSet对象中
+            pstmt.executeBatch();
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+                reader.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+}
