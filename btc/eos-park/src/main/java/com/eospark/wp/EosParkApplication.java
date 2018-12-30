9642ce060f1428ff9da61a534cae44e2f8624e47
weipeng <weipeng@symdata.cn>
2018-12-30 20:11:13 +0800

    提交eos

---
+++ btc/eos-park/src/main/java/com/eospark/wp/EosParkApplication.java
@@ -0,0 +1,17 @@
+package com.eospark.wp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author weipeng
+ */
+@SpringBootApplication
+public class EosParkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EosParkApplication.class, args);
+    }
+
+}
+
---
+++ btc/eos-park/src/main/java/com/eospark/wp/controller/EosController.java
@@ -0,0 +1,13 @@
+package com.eospark.wp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author weipeng
+ * @date 2018/12/30 20:09
+ */
+@RestController
+@RequestMapping("/eos/wp")
+public class EosController {
+}
